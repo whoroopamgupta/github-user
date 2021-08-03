b09a1de93db033f43e87a4e9730937227ccb8d6c
whoroopamgupta <roopamgupta68@gmail.com>
2021-08-04 02:17:20 +0530

    first projects

---
+++ src/context/context.js
@@ -0,0 +1,114 @@
+import React, { useState, useEffect } from "react";
+import mockUser from "./mockData.js/mockUser";
+import mockRepos from "./mockData.js/mockRepos";
+import mockFollowers from "./mockData.js/mockFollowers";
+import axios from "axios";
+
+const rootUrl = "https://api.github.com";
+
+const GithubContext = React.createContext();
+
+// Provider Consumer - GithubContext.Provider
+
+const GithubProvider = ({ children }) => {
+  const [githubUser, setGithubUser] = useState(mockUser);
+  const [repos, setRepos] = useState(mockRepos);
+  const [followers, setFollowers] = useState(mockFollowers);
+  // request loading
+  const [requests, setRequests] = useState(0);
+  const [isLoading, setIsLoading] = useState(false);
+
+  //error
+  const [error, setError] = useState({ show: false, msg: "" });
+
+  const searchGithubUser = async (user) => {
+    // console.log(user);
+    // toggleError
+    setIsLoading(true);
+    const response = await axios(`${rootUrl}/users/${user}`).catch((err) =>
+      console.log(err)
+    );
+    if (response) {
+      setGithubUser(response.data);
+      const { login, followers_url } = response.data;
+      // //repose
+      // await axios(`${rootUrl}/users/${login}/repos?per_page=100`).then(
+      //   (response) =>
+      //     // console.log(response)
+      //     setRepos(response.data)
+      // );
+      // //followers
+      // await axios(`${followers_url}?per_page=100`).then((response) =>
+      //   // console.log(response)
+      //   setFollowers(response.data)
+      // );
+      await Promise.allSettled([
+        axios(`${rootUrl}/users/${login}/repos?per_page=100`),
+        axios(`${followers_url}?per_page=100`),
+      ])
+        .then((results) => {
+          // console.log(results);
+          const [repos, followers] = results;
+          const status = "fulfilled";
+          if (repos.status === status) {
+            setRepos(repos.value.data);
+          }
+          if (followers.status === status) {
+            setFollowers(followers.value.data);
+          }
+        })
+        .catch((err) => console.log(err));
+      // more logic...
+      // repos
+      // https://api.github.com/users/john-smilga/repos?per_page=100
+      // followers
+      // https://api.github.com/users/john-smilga/followers
+    } else {
+      toggleError(true, "there is no user with that username");
+    }
+    checkRequest();
+    setIsLoading(false);
+  };
+
+  // check rate
+  const checkRequest = () => {
+    axios(`${rootUrl}/rate_limit`)
+      .then(({ data }) => {
+        // console.log(data);
+        let {
+          rate: { remaining },
+        } = data;
+        // remaining = 0;
+        setRequests(remaining);
+        if (remaining === 0) {
+          // throw an error
+          toggleError(true, "sorry, you have exceeded your hourly rate limit");
+        }
+      })
+      .catch((err) => console.log(err));
+  };
+
+  function toggleError(show = false, msg = "") {
+    setError({ show, msg });
+  }
+
+  // error
+  useEffect(checkRequest, []);
+  return (
+    <GithubContext.Provider
+      value={{
+        githubUser,
+        repos,
+        followers,
+        requests,
+        error,
+        searchGithubUser,
+        isLoading,
+      }}
+    >
+      {children}
+    </GithubContext.Provider>
+  );
+};
+
+export { GithubProvider, GithubContext };
---
+++ src/index.js
@@ -0,0 +1,31 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { GithubProvider } from "./context/context";
+import { Auth0Provider } from "@auth0/auth0-react";
+
+// devroopam.us.auth0.com
+// ZmZ6RhlboKWXL0LPeszHGuXPudqDRU8R
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Auth0Provider
+      domain="devroopam.us.auth0.com"
+      clientId="ZmZ6RhlboKWXL0LPeszHGuXPudqDRU8R"
+      redirectUri={window.location.origin}
+      cacheLocation="localstorage"
+    >
+      <GithubProvider>
+        <App />
+      </GithubProvider>
+    </Auth0Provider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
